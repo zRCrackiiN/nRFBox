a9dbd963bbf5d0e26bf4d7c04eaeeaa49fdbca73
jbohack <joebohack@gmail.com>
2025-04-18 10:22:53 -0400

    Flipper Zero Scanning
    Detected nearby Flipper Zeros using the vendor MAC ADDR

--- VScode Platformio/src/nRFBox_V2.ino
+++ VScode Platformio/src/nRFBox_V2.ino
@@ -26,6 +26,7 @@
 #include "blescan.h"
 #include "wifiscan.h"
 #include "blackout.h"
+#include "flipper.h"
 
 
 #define CE_PIN_A  5
@@ -49,7 +50,7 @@ Adafruit_NeoPixel pixels(1, 14, NEO_GRB + NEO_KHZ800);
 extern uint8_t oledBrightness;
 
 
-const unsigned char* bitmap_icons[11] = {
+const unsigned char* bitmap_icons[12] = {
   bitmap_icon_scanner,
   bitmap_icon_analyzer,
   bitmap_icon_jammer,
@@ -58,13 +59,14 @@ const unsigned char* bitmap_icons[11] = {
   bitmap_icon_spoofer,
   bitmap_icon_apple,
   bitmap_icon_ble,
+  bitmap_icon_ble,
   bitmap_icon_wifi,
   bitmap_icon_about,
   bitmap_icon_setting
 };
 
 
-const int NUM_ITEMS = 11; 
+const int NUM_ITEMS = 12;
 const int MAX_ITEM_LENGTH = 20; 
 
 char menu_items [NUM_ITEMS] [MAX_ITEM_LENGTH] = {  
@@ -76,6 +78,7 @@ char menu_items [NUM_ITEMS] [MAX_ITEM_LENGTH] = {
   { "BLE Spoofer" }, 
   { "Sour Apple" },
   { "BLE Scan" },
+  { "Flipper Scan" },
   { "WiFi Scan" },
   { "About" },
   { "Setting" }
@@ -149,10 +152,10 @@ void setup() {
   u8g2.print("by CiferTech");
 
   u8g2.setFont(u8g2_font_6x10_tf); 
-  int16_t versionWidth = u8g2.getUTF8Width("v2.5.0");
+  int16_t versionWidth = u8g2.getUTF8Width("v2.6.0");
   int16_t versionX = (128 - versionWidth) / 2;
   u8g2.setCursor(versionX, 60);
-  u8g2.print("v2.5.0");
+  u8g2.print("v2.6.0");
   
   u8g2.sendBuffer(); 
   delay(3000);
@@ -205,9 +208,9 @@ void loop() {
      button_select_clicked = 1; 
 
 
-if (current_screen == 0 && item_selected == 10) {
+if (current_screen == 0 && item_selected == 11) {
   settingSetup();
-    while (item_selected == 10) {
+    while (item_selected == 11) {
         if (digitalRead(BUTTON_SELECT_PIN) == HIGH) {
             if (callAbout) {
                 settingLoop();
@@ -228,8 +231,8 @@ if (current_screen == 0 && item_selected == 10) {
   }     
 
 
-if (current_screen == 0 && item_selected == 9) {
-    while (item_selected == 9) {
+if (current_screen == 0 && item_selected == 10) {
+    while (item_selected == 10) {
         if (digitalRead(BUTTON_SELECT_PIN) == HIGH) {
             if (callAbout) {
                 about();
@@ -250,9 +253,9 @@ if (current_screen == 0 && item_selected == 9) {
   }
 
 
-if (current_screen == 0 && item_selected == 8) {
+if (current_screen == 0 && item_selected == 9) {
   wifiscanSetup();
-    while (item_selected == 8) {
+    while (item_selected == 9) {
         if (digitalRead(BUTTON_SELECT_PIN) == HIGH) { 
          wifiscanLoop();     
             if (callAbout) {                             
@@ -273,6 +276,17 @@ if (current_screen == 0 && item_selected == 8) {
     }
 }
 
+if (current_screen == 0 && item_selected == 8) {
+  flipperSetup();
+  while (item_selected == 8) {
+    flipperLoop();
+
+    if (digitalRead(BUTTON_SELECT_PIN) == LOW) {
+      while (digitalRead(BUTTON_SELECT_PIN) == LOW);
+      break;
+    }
+  }
+}
 
 if (current_screen == 0 && item_selected == 7) {
   blescanSetup();
